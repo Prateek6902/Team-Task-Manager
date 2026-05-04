81ee5289df12bea45a0be820cff71586c5fff7da
Prateek singh gurjar <166498130+Prateek6902@users.noreply.github.com>
2026-05-04 21:48:58 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,41 +7,31 @@ const generateToken = (id) =>
 exports.register = async (req, res) => {
   const user = await User.create(req.body);
   const token = generateToken(user._id);
-  res.json({ token, user });
+
+  res.json({
+    success: true,
+    data: { token, user }
+  });
 };
 
 exports.login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email }).select('+password');
 
   if (!user || !(await user.comparePassword(req.body.password))) {
-    return res.status(401).json({ message: 'Invalid credentials' });
+    return res.status(401).json({ success: false, message: 'Invalid credentials' });
   }
 
   const token = generateToken(user._id);
-  res.json({ token, user });
+
+  res.json({
+    success: true,
+    data: { token, user }
+  });
 };
 
 exports.getMe = async (req, res) => {
-  res.json({ user: req.user });
-};
-
-exports.logout = async (req, res) => {
-  res.json({ message: 'Logged out' });
-};
-
-exports.updateDetails = async (req, res) => {
-  const user = await User.findByIdAndUpdate(req.user.id, req.body, { new: true });
-  res.json({ user });
-};
-
-exports.updatePassword = async (req, res) => {
-  const user = await User.findById(req.user.id).select('+password');
-
-  const match = await user.comparePassword(req.body.currentPassword);
-  if (!match) return res.status(400).json({ message: 'Wrong password' });
-
-  user.password = req.body.newPassword;
-  await user.save();
-
-  res.json({ message: 'Password updated' });
+  res.json({
+    success: true,
+    data: req.user
+  });
 };
