7256d0eb97b30c270ba8a421fe59a2c39e6d871b
Prateek singh gurjar <166498130+Prateek6902@users.noreply.github.com>
2026-05-04 22:13:38 +0530

    Update projects.js

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -13,28 +13,18 @@ const {
 
 const { protect, authorize } = require('../middleware/auth');
 
-// All routes require login
 router.use(protect);
 
-// ================= ROUTES =================
-
-// Get all projects / Create project
 router.route('/')
   .get(getProjects)
   .post(createProject);
 
-// Get / Update / Delete single project
 router.route('/:id')
   .get(getProject)
   .put(authorize('admin'), updateProject)
   .delete(authorize('admin'), deleteProject);
 
-// ================= MEMBERS =================
-
-// Add member
 router.post('/:id/members', authorize('admin'), addMember);
-
-// Remove member
 router.delete('/:id/members/:userId', authorize('admin'), removeMember);
 
 module.exports = router;
